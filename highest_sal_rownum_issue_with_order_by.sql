7ae04237684b3d22ab6a8188be63071d81711ac3
AnekGit <anekkumarsingh01@outlook.com>
2019-07-22 23:19:44 +0530

    caveats with rownum while ordering

--- highest_sal_rownum_issue_with_order_by.sql
+++ highest_sal_rownum_issue_with_order_by.sql
@@ -17,11 +17,42 @@ insert into employees_t values (SEQ.nextval,4000,10);
 insert into employees_t values (SEQ.nextval,5000,20);
 insert into employees_t values (SEQ.nextval,6000,30);
 commit;
-select * from employees_t;
+explain plan for select * from employees_t;
 
 --First select clause is executed then order by salary gets 
 --applied NOTE:Output
+--6000	6
+--5000	5
+--4000	4
+--3000	3
+--2000	2
+--1000	1
 select salary,rownum from employees_t order by salary desc ;
+-- to get the output in which rownum is ordered correctly 
+-- use this 
+select salary ,rownum as ranking from 
+(select salary from employees_t order by salary desc)
+;
+
+explain plan for select salary,rownum from employees_t order by salary desc ;
+select * from table(DBMS_XPLAN.DISPLAY);
+
+--2nd highest salary
+select max(t1.salary) from employees_t t1  where salary NOT IN 
+(select max(salary) from employees_t ) ;
+
+--3rd hightest salary 
+
+select salary ,rownum as ranking from 
+(select salary from employees_t order by salary desc)
+;
+
+
+
+
+
+
+
 
 
 
