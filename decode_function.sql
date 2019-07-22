1e26cf7e822fc23daee206f9d01cff9cbae1ee17
AnekGit <anekkumarsingh01@outlook.com>
2019-07-22 23:45:08 +0530

    decode caveats

---
+++ decode_function.sql
@@ -0,0 +1,20 @@
+-- convert rows to columns
+
+create table rows_table(
+col1 number ,
+col2 number );
+insert into rows_table values (1,4);
+insert into rows_table values (2,5);
+insert into rows_table values (3,6);
+commit;
+select col1,col2 from rows_table;
+
+select col1,col2,DECODE(col2,4,4,
+                             2,2,
+                             3,3
+                         ) as col3 from rows_table;
+                         
+-- check is done 
+--using null is null condn thats y output is NULL DATA
+select DECODE(NULL,NULL, 'NULL DATA',
+                  'NOT NULL') from dual ;                         
